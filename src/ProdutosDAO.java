ad7a17f58337e4d545d2dda2771cacf564954139
Tiago Rech <rech.tiago94@gmail.com>
2025-06-14 16:57:41 -0300

    Implementando metodo venderProduto no DAO

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -65,6 +65,42 @@ public class ProdutosDAO {
         return listagem;
     }
     
+    public void venderProduto(int id) {
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, id);
+            prep.executeUpdate();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + e.getMessage());
+        }
+    }
+    
+        public ArrayList<ProdutosDTO> listarProdutosVendidos(){
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido' ";
+        ArrayList<ProdutosDTO> vendidos = new ArrayList<>();
+        
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                vendidos.add(produto);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
+        }
+        return vendidos;
+    }
+    
     
     
         
